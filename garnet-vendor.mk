d80e8914d08134529730c55072e809edbd9189ed
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-03-04 18:14:24 -0300

    garnet: Introduce DolbyManager
    Based on existing dirac implementation and observing stock
    sound effects app and daxservice.
    
    History -
    - Add translation pt-BR
    - Fix compilation error in TooltipIconButton
    - Switch to MaterialTheme settingsBackground for EqualizerScreen background color
    - Allow bass enhancer to use in speaker also
    - Remove deprecated PlainTooltipBox
    - Fix build with kotlinc 1.9.0
    - Add launcher icon
    - Introduce graphical equalizer
    - Do not set volume leveler amount
    - Restore all settings upon bootup
    - Rewrite in Kotlin
    - Restore dolby profile on audio changes
    - Guard debug logging
    - Implement profile-specific Dolby settings
    - Remove play/pause hack while toggling Dolby
    - Set proper summary for dolby settings
    - Always refresh playback if status changed
    - Introduce Dolby Atmos
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Co-authored-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -531,6 +531,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/garnet/proprietary/vendor/etc/init/qseecomd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/qseecomd.rc \
     vendor/xiaomi/garnet/proprietary/vendor/etc/init/ssgtzd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/ssgtzd.rc \
     vendor/xiaomi/garnet/proprietary/vendor/etc/init/vendor.display.color@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.display.color@1.0-service.rc \
+    vendor/xiaomi/garnet/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
+    vendor/xiaomi/garnet/proprietary/vendor/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.media.c2@1.0-service.rc \
     vendor/xiaomi/garnet/proprietary/vendor/etc/init/vendor.dpmd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dpmd.rc \
     vendor/xiaomi/garnet/proprietary/vendor/etc/init/vendor.qti.adsprpc-guestos-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.adsprpc-guestos-service.rc \
     vendor/xiaomi/garnet/proprietary/vendor/etc/init/vendor.qti.camera.provider@2.7-service_64.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.camera.provider@2.7-service_64.rc \
@@ -1291,6 +1293,10 @@ PRODUCT_PACKAGES += \
     libcneapiclient \
     libcneoplookup \
     libcneqmiutils \
+    libcodec2_soft_ac4dec \
+    libcodec2_soft_ddpdec \
+    libcodec2_soft_dolby \
+    libcodec2_store_dolby \
     libcom.qti.chinodeutils \
     libcom.xiaomi.debug \
     libcom.xiaomi.grallocutils \
@@ -1301,13 +1307,17 @@ PRODUCT_PACKAGES += \
     libcpion \
     libcv_common \
     libcvp2_1 \
+    libdapparamstorage \
     libdataitems \
+    libdeccfg \
     libdeflicker \
     libdiag \
     libdigital-dimming \
     libdisp-aba \
     libdisplayqos \
     libdisplayskuutils \
+    libdlbdsservice \
+    libdlbpreg \
     libdlrmsc \
     libdpmqmihal \
     libdpps \
@@ -1619,6 +1629,10 @@ PRODUCT_PACKAGES += \
     sensors.touch.detect \
     sensors.ultrasoundproximity \
     libasphere \
+    libdlbvol \
+    libswdap \
+    libswgamedap \
+    libswvqe \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
     vendor.display.color@1.2 \
@@ -1628,6 +1642,8 @@ PRODUCT_PACKAGES += \
     vendor.display.color@1.6 \
     vendor.display.color@1.7 \
     vendor.display.postproc@1.0 \
+    vendor.dolby.hardware.dms@2.0-impl \
+    vendor.dolby.hardware.dms@2.0 \
     vendor.libdpmctmgr \
     vendor.libdpmfdmgr \
     vendor.libdpmframework \
@@ -1880,8 +1896,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss-aidl-service-qti.xml \
     android.hardware.gnss@2.1-service-qti.xml \
     android.hardware.security.keymint-service-qti-rkp.xml \
-    c2_manifest_vendor.xml \
-    c2_manifest_vendor_audio.xml \
     dataconnection-saidl.xml \
     manifest_android.hardware.drm@1.4-service.widevine.xml \
     manifest_non_qmaa.xml \
@@ -1918,6 +1932,8 @@ PRODUCT_PACKAGES += \
     android.hardware.security.keymint-service-qti \
     qcrilNrd \
     vendor.display.color@1.0-service \
+    vendor.dolby.hardware.dms@2.0-service \
+    vendor.dolby.media.c2@1.0-service \
     vendor.qti.camera.provider@2.7-service_64 \
     vendor.qti.esepowermanager@1.1-service \
     vendor.qti.hardware.alarm@1.0-service \
